108586b0bd089572056e7deeff482cfd7edfaaf0
ml-astro <72692667+ml-astro@users.noreply.github.com>
2024-01-29 18:19:52 +0200

    Add files via upload

--- dictionary.js
+++ dictionary.js
@@ -3,7 +3,7 @@ function showAll(){
 	for (const element of dictionary) {
 		let text = element + ''
 		let translation = text.split(" - ")
-		document.getElementById("table").innerHTML+=`<tr><td>${translation[0]}</td><td>${translation[1]}</td></tr>`
+		document.getElementById("table").innerHTML+=`<tr><td><a href="https://www.eki.ee/dict/evs/index.cgi?Q=${translation[0]}" target="_blank">${translation[0]}</a></td><td>${translation[1]}</td></tr>`
 	}
 	document.getElementById("table").innerHTML+="</table>"
 	document.getElementById("table").style.textAlign="left"
